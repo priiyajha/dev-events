f2494342f0cb51f55d761446d6dac88f23de9d16
priiyajha <jhapriiyaa2104@gmail.com>
2025-10-26 21:13:03 +0530

    home page and layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div>Welcome Page</div>
+        <section>
+            <h1 className="text-center">
+                The Hub For Every Dev <br/> Event You Can't Miss
+            </h1>
+            <p className="text-center mt-5">Hackathons, Meetups and Conferences All In One Place</p>
+        </section>
     )
 }
 export default Page
